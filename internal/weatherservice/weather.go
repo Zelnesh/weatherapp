4ed7a73e0b81abd6a11d31e1eaea85f6c5081f86
Zelnesh <zelnesh7777@gmail.com>
2026-04-12 23:15:19 +0000

    Added client, agent header and error handling for weather API service

--- internal/weatherservice/weather.go
+++ internal/weatherservice/weather.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"encoding/json"
+	"time"
 )
 
 type WeatherResponse struct {
@@ -18,10 +19,28 @@ type WeatherCurrent struct {
 
 func GetCurrentWeather(latitude, longitude float64) (*WeatherResponse, error) {
 	url := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%.2f&longitude=%.2f&current=temperature_2m", latitude, longitude)
-	resp, err := http.Get(url)
+
+	client := &http.Client {
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
+
+	req.Header.Set("User-Agent", "weather-app")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		err := fmt.Errorf("Weather service API bad response code: %s", resp.Status)
+		return nil, err
+	}
+
 	defer resp.Body.Close()
 
 	var weather WeatherResponse
